bf6cca93ed02359f2331437416a9538b76805a7c
ntrhome <ntrhome@ukr.net>
2024-01-02 12:54:59 +0200

    thread-0-main

--- main.c
+++ main.c
@@ -1,23 +1,42 @@
 //#include "durModel.h"
 #include "durView.h"
 #include <stdio.h>
-#include <pthread.h>
+#include <pthread.h> //for pthread_create()... man 7 pthreads
+#include <unistd.h>  //for sleep(). man 3 sleep
 
-typedef
 
-void *start_thread(void *message) {
-    printf ("%s\n", (const char *) message);
-    return message;
+typedef struct DUR_S_CTRL {
+    char *s;
+} dur_s_ctrl;
+pthread_mutex_t mutex;
+
+void *newThread(void *arg) {
+    dur_s_ctrl *a = arg;
+    //printf ("%s\n", ((dur_s_ctrl*)arg)->s);
+    pthread_mutex_lock(&mutex);
+    printf("[%lu] s=%s. I lock mutex.\n", pthread_self(), a->s);
+    printf ("[%lu] I fall asleep for 3 seconds.\n", pthread_self());
+    sleep(3);
+    printf ("[%lu] I wake up! I unlock mutex.\n", pthread_self());
+    pthread_mutex_unlock(&mutex);
+    return arg; //return NULL;
 }
 
-int main(int argc, char *argv[], char **penv) {
+int main(int argc, char *argv[], char *envp[]) {
     //dur();
-    pthread_t thing1, thing2;
-    const char *message1 = "Thing 1";
-    const char *message2 = "Thing 2";
-    pthread_create (&thing1, NULL, start_thread, (void *) message1);
-    pthread_create (&thing2, NULL, start_thread, (void *) message2);
-    pthread_join (thing1, NULL);
-    pthread_join (thing2, NULL);
+    pthread_mutex_init(&mutex,NULL);
+    dur_s_ctrl arg1 = {"THREAD A"};
+    dur_s_ctrl arg2 = {"THREAD B"};
+    pthread_t ctrl1, ctrl2; //структуры - инфа о потоках
+    if(pthread_create (&ctrl1, NULL, newThread, (void *) &arg1) != 0){printf("error1\n");}
+    if(pthread_create (&ctrl2, NULL, newThread, (void *) &arg2) != 0){printf("error2\n");}
+
+    printf("[%lu] MAIN. I join to threads.\n", pthread_self());
+    pthread_join (ctrl1, NULL); //ждем завершения потока
+    pthread_join (ctrl2, NULL);
+
+    printf ("%s\n", "MAIN. kurbasa!");
+    pthread_mutex_destroy(&mutex);
+
     return 0;
 }
