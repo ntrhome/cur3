4bdbf9772ece40120d84fca39e3a0f9c19e56aba
ntrhome <ntrhome@ukr.net>
2024-01-25 15:43:21 +0200

    next

--- durControl.c
+++ durControl.c
@@ -3,45 +3,45 @@
 #include "durView.h"
 #include "durControl.h"
 
-static es durControl_inputCardOrCommand() { //return card [0..ed_cards) or command (es > 1000)
-    fflush(stdout);
-    fflush (stdin);
-    char s[2+1];
-    int i = scanf("%2s", s); //printf("i=%d, s=%s, s[0]=%d, s[1]=%d, s[2]=%d.\n", i, s, s[0],s[1],s[2]); //dbg
-    if (i != 1) return es_cmd_wrong;
-    if (s[0] > 96 && s[0] < 123) s[0] -= ('a' - 'A'); //toupper(s[0])
-    //command
-    if (s[1] == 0) {
-        if(s[0] == 'T') return es_cmd_take;
-        if(s[0] == 'E') return es_cmd_enough;
-        if(s[0] == 'Q') return es_cmd_quit;
-        return es_cmd_wrong;
-    }
-    //card
-    int rank = 0;
-    for (; rank < ed_ranks; ++rank) {
-        if (s[0] == ranks[rank]) break;
-    }
-    if (rank == ed_ranks) return es_cmd_wrong;
-    if (s[1] > 64 && s[1] < 91)  s[1] += ('a' - 'A'); //tolower(s[0])
-    int suit = 0;
-    for (; suit < ed_suits; ++suit) {
-        if (s[1] == suits[suit]) break;
-    }
-    if (suit == ed_suits) return es_cmd_wrong;
-    return suit * ed_ranks + rank; //card
-}
-
-static void durControl_attack(sBoard *b) {
-    b->cmd = durControl_inputCardOrCommand();
-    b->stage = es_attackResult;
-}
+//static es durControl_inputCardOrCommand() { //return card [0..ed_cards) or command (es > 1000)
+//    fflush(stdout);
+//    fflush (stdin);
+//    char s[2+1];
+//    int i = scanf("%2s", s); //printf("i=%d, s=%s, s[0]=%d, s[1]=%d, s[2]=%d.\n", i, s, s[0],s[1],s[2]); //dbg
+//    if (i != 1) return es_cmd_wrong;
+//    if (s[0] > 96 && s[0] < 123) s[0] -= ('a' - 'A'); //toupper(s[0])
+//    //command
+//    if (s[1] == 0) {
+//        if(s[0] == 'T') return es_cmd_take;
+//        if(s[0] == 'E') return es_cmd_enough;
+//        if(s[0] == 'Q') return es_cmd_quit;
+//        return es_cmd_wrong;
+//    }
+//    //card
+//    int rank = 0;
+//    for (; rank < ed_ranks; ++rank) {
+//        if (s[0] == ranks[rank]) break;
+//    }
+//    if (rank == ed_ranks) return es_cmd_wrong;
+//    if (s[1] > 64 && s[1] < 91)  s[1] += ('a' - 'A'); //tolower(s[0])
+//    int suit = 0;
+//    for (; suit < ed_suits; ++suit) {
+//        if (s[1] == suits[suit]) break;
+//    }
+//    if (suit == ed_suits) return es_cmd_wrong;
+//    return suit * ed_ranks + rank; //card
+//}
+//
+//static void durControl_attack(sBoard *b) {
+//    b->cmd = durControl_inputCardOrCommand();
+//    b->stage = es_attackResult;
+//}
 
 void durControl(sBoard *b) {
-    //printf("durControl\n");
-    switch (b->stage) {
-        case es_attackControl:
-            durControl_attack(b);
-            break;
-    }
+    printf("durControl\n");
+//    switch (b->stage) {
+//        case es_attackControl:
+//            durControl_attack(b);
+//            break;
+//    }
 }
